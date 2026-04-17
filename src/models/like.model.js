dbab56174f9838d01be1bb5078450c85e3808b20
Bhavya Jain <jainbhavya0527@gmail.com>
2026-04-17 12:05:39 +0530

    Add models for comments, likes, playlists, and tweets with Mongoose schemas

---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+    {
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        tweet: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tweet"
+        },
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        likedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+export { Like };
